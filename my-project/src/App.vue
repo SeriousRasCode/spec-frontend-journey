908dca3c778987bce7d97b390cf35c3af605057c
Tedros Teshome <tedrhanna@gmail.com>
2026-02-04 10:04:10 +0300

    input forms in vue using ref

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,3 +1,9 @@
-<h1>Hello SPEC Developers</h1>
+<script setup>
+import { ref } from 'vue'
+const name = ref('')
+</script>
 
-
+<template>
+  <input v-model="name" placeholder="Type your name">
+  <p>Hello {{ name }}</p>
+</template>
