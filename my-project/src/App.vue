299fa01a0fcf37b835ac57e3ddab6a05d44b0b19
Tedros Teshome <tedrhanna@gmail.com>
2026-02-05 19:48:35 +0300

    remove emoji indicators for online/offline status, and export default script in app.vue

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -2,6 +2,9 @@
 import { ref } from 'vue'
 import UserCard from './components/UserCard.vue'
 const name = ref('')
+
+
+
 </script>
 
 <template>
@@ -15,8 +18,8 @@ const name = ref('')
   </li>
 </ul>-->
 
-<!--day 2 challenge -->
-<!--<h1 v-if="name">
+  <!--day 2 challenge -->
+  <!--<h1 v-if="name">
     Greetings {{ name }}!
   </h1><br><br>
 <input v-model="name" placeholder="Enter your name">
@@ -27,16 +30,23 @@ const name = ref('')
     አትፈር
   </h1>
   -->
-<!--day 3 challenge -->
-<div class="card-cont">
-  <UserCard name="Tedros" role="Admin" status="online" 
-      @view-profile="showAlert"/>
-  <UserCard name="Moa" role="Manager" status="offline" 
-      @view-profile="showAlert"/>
-  <UserCard name="Ezana" role="Employee" status="online" 
-      @view-profile="showAlert"/>
+  <!--day 3 challenge -->
+  <div class="card-cont">
+    <UserCard name="Tedros" role="Admin" status="online" @view-profile="showAlert" />
+    <UserCard name="Moa" role="Manager" status="offline" @view-profile="showAlert" />
+    <UserCard name="Ezana" role="Employee" status="online" @view-profile="showAlert" />
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    showAlert(userName) {
+      alert("Opening profile for: " + userName);
+    }
+  }
+}
+</script>
 
 <style lang="css">
 .card-cont {
