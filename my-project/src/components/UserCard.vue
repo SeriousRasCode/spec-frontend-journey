0e7fb6e3eee0fba849e646bd32656bb44ac9de67
Tedros Teshome <tedrhanna@gmail.com>
2026-02-05 20:11:17 +0300

    remove status text from UserCard component

--- my-project/src/components/UserCard.vue
+++ my-project/src/components/UserCard.vue
@@ -2,8 +2,8 @@
   <!--div with conditional coloring using ternary operator-->
   <div class="card" :style="{ borderColor: status === 'online' ? 'green' : 'red' }">
 
-    <p v-if="status === 'online'">🟢 Online</p>
-    <p v-else>🔴 Offline</p>
+    <p v-if="status === 'online'">🟢</p>
+    <p v-else>🔴</p>
 
     <h1>{{ name }}</h1>
     <p>{{ role }}</p>
